a83fdf44705377bb721f5d283c521b74e637f0c5
webEthiopia <buiearn45@gmail.com>
2025-06-12 08:58:08 -0700

    Create MainActivity.java

---
+++ Toast password display/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText pass_word;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        addListenerToButton();
+    }
+
+    public void addListenerToButton(){
+        pass_word = (EditText) findViewById(R.id.editText);
+        Button button_sbm = (Button) findViewById(R.id.button);
+        button_sbm.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(
+                                MainActivity.this, pass_word.getText(), Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                }
+        );
+    }
+
+
+}
