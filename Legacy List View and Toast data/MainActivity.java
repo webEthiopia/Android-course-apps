f821013dea457ae1bb50a813c0cd1652f26949c0
webEthiopia <buiearn45@gmail.com>
2025-06-15 08:05:04 -0700

    Create MainActivity.java

---
+++ Legacy List View and Toast data/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.helloworld;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static ListView list_view;
+    private static String[] names= new String[] {"Tom" , "Mark" , "John" , "Jack" , "Patrick" , "Peter"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        listView();
+    }
+
+    public void listView(){
+        list_view = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.name_list,names);
+        list_view.setAdapter(adapter);
+
+        list_view.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String value = (String) list_view.getItemAtPosition(position);
+                        Toast.makeText(MainActivity.this,"Position: " + (position + 1) + ", Value: " + value,Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+}
