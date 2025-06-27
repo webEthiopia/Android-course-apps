f6b8fc98e949ff84b7f2ec80dae529408b205989
webEthiopia <buiearn45@gmail.com>
2025-06-27 11:33:44 -0700

    Create MainActivity.java

---
+++ auto complete text app/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+  private AutoCompleteTextView acTextView;
+   String[] Countries = {
+        "Afghanistan",
+        "Albania",
+        "Algeria",
+        "Andorra",
+        "Angola",
+        "Antigua and Barbuda",
+        "Argentina",
+        "Armenia",
+        "Australia",
+        "Austria",
+        "Azerbaijan"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+      acTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+      ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,Countries);
+
+      acTextView.setThreshold(1);
+      acTextView.setAdapter(adapter);
+    }
+
+   
+}
